d76da004388a98e06579ef0e951eb41704f337af
Adi <adids1221@gmail.com>
2020-09-07 12:13:28 +0300

    Refactor to MVC Model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,17 +1,6 @@
-const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
-
-exports.checkID = (req, res, next, val) => {
-    console.log(`This is the id: ${val}`);
-    if(req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status:'failed' ,
-            message: `Failed to find tour id:${req.params.id * 1}`
-        });
-    }
-    next();
-}
+//const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
 exports.checkBody = (req, res, next) => {
     if(!req.body.name || !req.body.price){
@@ -27,10 +16,10 @@ exports.getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         requestTime: req.requestTime,
-        results: tours.length,
+       /*  results: tours.length,
         data: {
             tours
-        }
+        } */
     });
 };
 
@@ -38,29 +27,22 @@ exports.getTour = (req, res) => { //parameter => :id || optinal parameter => :id
     console.log(req.params);//req.params assign the value to the parameter => :id
 
     const id = req.params.id * 1; //convert from string to int
-    const tour = tours.find(el => el.id === id);
+    /* const tour = tours.find(el => el.id === id);
 
     res.status(200).json({
     status: 'success',
     data: {
         tour
     } 
-    });
+    }); */
 };
 
 exports.createTour = (req, res) =>{ //post => send data from the client/req to the server || add new tour
-    const newId = ((tours[tours.length-1].id) + 1);
-    console.log(newId);
-    const newTour = Object.assign({id: newId}, req.body);
-
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({ //HTTP status 201 gor create
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        });
+    res.status(201).json({ //HTTP status 201 gor create
+        status: 'success',
+        /* data: {
+            tour: newTour
+        } */
     });
 };
 
--- server.js
+++ server.js
@@ -13,36 +13,7 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
-}).then(con => console.log(`DB connection successful!`));
-
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'], //validator
-        unique: true
-    },
-    rating: {
-        type: Number,
-        default: 4.5
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
-    }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-    name: "The Park Camper",
-    price: 997
-});
-
-testTour.save().then(doc => {
-    console.log(doc);
-}).catch(err => {
-    console.log(`error: ${err}`);
-});
+}).then(() => console.log(`DB connection successful!`));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
