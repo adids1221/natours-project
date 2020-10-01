248e66a71d5b9a452753cfb9057e6a107a4084c9
Adi Mordo <adids1221@gmail.com>
2020-10-01 12:52:31 +0300

    Added calculating average rating middleware on tours

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -78,7 +78,8 @@ exports.getAll = Model =>
             .sort()
             .limitFields()
             .paginate();
-        const doc = await features.query.explain();
+        //const doc = await features.query.explain();
+        const doc = await features.query;
 
         //Send response 
         res.status(200).json({
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
     {
@@ -37,15 +38,70 @@ const reviewSchema = new mongoose.Schema(
 
 //Query middleware
 reviewSchema.pre(/^find/, function (next) {
-    this.populate({
+    /* this.populate({
         path: 'tour',
         options: { select: 'name' } 
     }).populate({
         path: 'user',
         options: { select: 'name photo' } 
+    }); */
+    this.populate({
+        path: 'user',
+        select: 'name photo'
     });
-
     next();
+});
+
+//Static Methods
+reviewSchema.statics.calcAverageRating = async function (tourId) {
+    console.log(tourId);
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+    //console.log(stats);
+
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantaity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantaity: 0,
+            ratingsAverage: 4.5
+        });
+    }
+};
+
+reviewSchema.post('save', function (next) {
+    //this points to the current review
+    //this.constructor points to the Model itself
+    //Using static method that can be used on the Model itself
+    this.constructor.calcAverageRating(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    //Using regex for FindOneAnd query functions -> Delete and Update
+    //this points to the current query
+    //Saving the query to this.r object for using in the next middleware
+    this.r = await this.findOne();
+    next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+    //Cant use this.findOne in the post middleware the query already executed
+    //Using this.r object passed from the pre middleware
+    //Post middleware on FindOneAnd query functions
+    await this.r.constructor.calcAverageRating(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
